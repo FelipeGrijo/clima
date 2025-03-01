4c9e07af6e519b3295c13e4b8b63ec3dd1a6dcf9
Felipe Grijó <24424864+FelipeGrijo@users.noreply.github.com>
2025-03-01 01:35:15 -0300

    Adiciona CSS do Flag Icons após carregar a página

--- src/js/main.js
+++ src/js/main.js
@@ -2,3 +2,11 @@ import updateWeather from './updateWeather.js';
 import './modal.js';
 
 updateWeather();
+
+// Adiciona o CSS do Flag Icons após carregar a página
+window.addEventListener('load', () => {
+  const link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'https://cdn.jsdelivr.net/gh/lipis/flag-icons@7.3.2/css/flag-icons.min.css';
+  document.head.appendChild(link);
+});
